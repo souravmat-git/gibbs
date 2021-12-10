270c280e746c698b17311a0439955fe3f42aae82
Sourav Chatterjee <schatterjee@Souravs-MacBook-Air.local>
2021-12-10 13:59:35 -0500

    Initial files

---
+++ src/base/gibbsApp.C
@@ -0,0 +1,53 @@
+#include "gibbsApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+InputParameters
+gibbsApp::validParams()
+{
+  InputParameters params = MooseApp::validParams();
+
+  // Do not use legacy material output, i.e., output properties on INITIAL as well as TIMESTEP_END
+  params.set<bool>("use_legacy_material_output") = false;
+
+  return params;
+}
+
+gibbsApp::gibbsApp(InputParameters parameters) : MooseApp(parameters)
+{
+  gibbsApp::registerAll(_factory, _action_factory, _syntax);
+}
+
+gibbsApp::~gibbsApp() {}
+
+void
+gibbsApp::registerAll(Factory & f, ActionFactory & af, Syntax & syntax)
+{
+  ModulesApp::registerAll(f, af, syntax);
+  Registry::registerObjectsTo(f, {"gibbsApp"});
+  Registry::registerActionsTo(af, {"gibbsApp"});
+
+  /* register custom execute flags, action syntax, etc. here */
+}
+
+void
+gibbsApp::registerApps()
+{
+  registerApp(gibbsApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+extern "C" void
+gibbsApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  gibbsApp::registerAll(f, af, s);
+}
+extern "C" void
+gibbsApp__registerApps()
+{
+  gibbsApp::registerApps();
+}
